db60d063407de458e0f1c272c92d6f2cf131233a
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-28 08:55:08 +0200

    scorrimento automatico 3 sec

--- js/main.js
+++ js/main.js
@@ -1,49 +1,3 @@
-/**
- *  Consegna:
-Dato un array contenente una lista di sei immagini (url), creare un carosello come nello screenshot allegato.
-
-MILESTONE 1
-Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
-
-MILESTONE 2
-Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
-
-Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-
-MILESTONE 3
-Al click dell'utente sui bottoni/frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
-
-BONUS 1:
-Aggiungere il ciclo infinito del carosello. Ovvero se è attiva la prima immagine e l'utente clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
-
-BONUS 2:
-Aggiungere la visualizzazione thumbnails, realizzate a piacere (pallini o miniature). Tutte le thumbnails avranno una classe inattiva, tranne quella corrispondente all’immagine attiva, che invece avrà una classe attiva (che la distinguerà con un bordo, un colore, ecc).
-Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
- */
-
-/**
- *
- *     // const imageToHide = imageElements[activeImageIndex];
-
-    // prendo l'immagine attiva =>  .carousel-image-container img.active
-    // const activeImage = document.querySelector('.carousel-image-container img.active ');
-    imageElements[activeImageIndex].classList.remove('active');
-
-    //  activeImageIndex = activeImageIndex + 1;
-    activeImageIndex++;
-
-    // se arrivo alla fine della lista ricomincio da zero
-    if (activeImageIndex === images.length) {
-        activeImageIndex = 0;
-    }
-
-    // prendo l'immagine all'indice attuale e le aggiungo la classe active per renderla visibile
-    imageElements[activeImageIndex].classList.add('active');
-
-    // console.log(imageElements);
- *
- */
-
 const images = [
     'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
     'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
@@ -53,7 +7,7 @@ const images = [
     'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'
 ];
 
-let activeImageIndex = 4;
+let activeImageIndex = 0;
 
 // recupero il parent all'interno del quale inserire le immagini
 const imagesWrapper = document.querySelector('.carousel-wrapper .carousel-image-container');
@@ -141,10 +95,11 @@ prevButton.addEventListener('click', function () {
 });
 
 
-// Aggiungete una funzionalità basica di scorrimento delle immagini: per cui ogni 3 secondi il carosello mostrerà l'immagine successiva senza bisogno di alcuna interazione da parte dell'utente.
+// Aggiungete una funzionalità basica di scorrimento delle immagini:
+// per cui ogni 3 secondi il carosello mostrerà l'immagine successiva
 
-const clock = setInterval(timeToSlide, 3000);
+const clock = setInterval(timetoSlide, 3000);
 
-function timeToSlide() {
-
+function timetoSlide() {
+    nextButton.click();
 }
